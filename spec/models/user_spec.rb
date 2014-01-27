b7dfb239f4fa26d1148fd63875f4c3b63a04ac32
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-01-27 13:32:56 -0700

    Update user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  let(:valid_attributes) {{ :user_id => 1,
-                            :title => "party!",
-                            :description => "new years",
-                            :venue => "my house",
-                            :address => "down the street 4",
-                            :starts_at => "2014-01-26 12:00:00" }}
-
   it "lists events with an rsvp" do
-    event1 = Event.create! valid_attributes
-    user = User.create(:email => "foo@bar.com",
-                                                     :password => "foobar!!",
-                                                     :password_confirmation => "foobar!!")
-    event1.attendees.create(:user_id => user.id, :event_id => event1.id)
-    rsvps = user.rsvps
-    rsvps.should eq([event1])
-
+    user1 = User.create!( :email => "foo@bar.com",
+                          :password => "foobar!!",
+                          :password_confirmation => "foobar!!")
+    
+    event = Event.create!( :user_id => user1.id,
+                            :title => "Football",
+                            :venue => "Cleveland Browns Stadium",
+                            :starts_at => "2014-04-26 12:00:00")
+    
+    user2 = User.create!(:email => "fizz@buzz.com",
+                          :password => "fizzbuzz!!",
+                          :password_confirmation => "fizzbuzz!!")
+    
+    event.attendees.create(:user_id => user2.id, :event_id => event.id)
+    rsvps = user2.rsvps
+    rsvps.should eq([event])
   end
 end
