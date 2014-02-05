9111eec397dec87ec0d5fbcf24002a127cc60737
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-02-04 19:50:40 -0700

    Setup sendgrid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
+  :address              => "smtp.sendgrid.net",
   :port                 => 587,
   :domain               => "fronnect.herokuapp.com",
-  :user_name            => "fronnect",
-  :password             => "cornell&duke",
+  :user_name            => ENV["sendgrid_username"],
+  :password             => ENV["sendgrid_password"],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
