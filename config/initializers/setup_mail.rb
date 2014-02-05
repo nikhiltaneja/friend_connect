ffd2fb15f023a25d8986e7374ed566385b93bfc5
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-02-04 20:02:39 -0700

    Add eager load for resque web

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.sendgrid.net",
   :port                 => 587,
   :domain               => "fronnect.herokuapp.com",
-  :user_name            => ENV["sendgrid_username"],
-  :password             => ENV["sendgrid_password"],
+  :user_name            => "ntaneja",
+  :password             => "bronco$",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Fronnect::Application.routes.draw do
 
   mount ResqueWeb::Engine => "/resque_web"
 
+  ResqueWeb::Engine.eager_load!
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
