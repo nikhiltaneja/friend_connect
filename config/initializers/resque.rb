ec632d9e943c145f5428d56a816eaca0cfc8f163
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-02-04 19:21:15 -0700

    Configure redistogo

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,10 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-Resque.redis = resque_config[rails_env]
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  resque_config = YAML.load_file(rails_root + '/config/resque.yml')
+  Resque.redis = resque_config[rails_env]
+end
